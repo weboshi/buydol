c0a7d4abb15dcf989bdf9a0afaf4d4850610b826
Le Andrew Nguyen <avident@gmail.com>
2025-08-16 17:02:17 -0700

    postcard finished
    users can comment, like, delete posts

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -55,8 +55,7 @@ export async function getUserByClerkId(clerkId:string) {
 
 export async function getDbUserId() {
     const {userId:clerkId} = await auth();
-    if(!clerkId) throw new Error("Unauthorized");
-
+    if(!clerkId) return null;
     const user = await getUserByClerkId(clerkId);
 
     if(!user) throw new Error("User not found");
@@ -67,6 +66,8 @@ export async function getDbUserId() {
 export async function getRandomUsers() {
     try {
         const userId = await getDbUserId();
+
+        if(!userId) return [];
 
         const randomUsers = await prisma.user.findMany({
             where: {
@@ -105,6 +106,8 @@ export async function getRandomUsers() {
 export async function toggleFollow(targetUserId:string) {
     try {
         const userId = await getDbUserId();
+
+        if(!userId) return;
 
         if (userId === targetUserId) throw new Error("You can't follow yourself");
 
@@ -155,3 +158,4 @@ export async function toggleFollow(targetUserId:string) {
         
     }
 }
+
