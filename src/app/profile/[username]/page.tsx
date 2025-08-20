758f9d76ffcdb4d690bc18fddfe5eaf0be98c30b
Le Andrew Nguyen <avident@gmail.com>
2025-08-20 16:14:11 -0700

    added notifications
    notifications for likes, follows and comments on user page

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ProfilePage({ params }: { params: { username: string } }) {
+    console.log("params:", params);
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ProfilePage 
